9aa47815f9f949cd7dc168909925d9ee4cc44d80
mauro-balades <mauro.balades@tutanota.com>
2022-06-28 13:29:47 +0200

    added random color generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { ColorWrapper } from "./styles"
+
+const COLORS = ["#f36747", "#f197a2",  "#4360a8","#3cc6ed","#842d73","#ee316b","#4152a5","#ffb137","#3cc6ed","#5ce5aa"]
 
 function App() {
+  const [color, setColor] = useState("");
+  const setRandomColor = () => {
+    let c = COLORS[Math.floor(Math.random()*COLORS.length)];
+    setColor(c);
+  }
+
+  useEffect(() => {
+    setRandomColor();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorWrapper color={color}></ColorWrapper>
   );
 }
 
---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+
+import styled from "styled-components";
+
+export const ColorWrapper = styled.main`
+    background: ${(props: any) => props.color};
+
+    width: 100%;
+    height: 100vh;
+
+
+`
