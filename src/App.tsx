f982ef3d2b04f772c5c74bf8eeea4b3aa21ef493
mauro-balades <mauro.balades@tutanota.com>
2022-06-28 14:07:36 +0200

    added ripples on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { ColorWrapper } from "./styles"
+import React, { Children, ReactElement, useEffect, useState } from 'react';
+import { ColorWrapper, RippleEffect } from "./styles"
 
 const COLORS = ["#f36747", "#f197a2",  "#4360a8","#3cc6ed","#842d73","#ee316b","#4152a5","#ffb137","#3cc6ed","#5ce5aa"]
 
 function App() {
   const [color, setColor] = useState("");
+  const [ripples, setRipples] = useState([] as any[]);
   const setRandomColor = () => {
     let c = COLORS[Math.floor(Math.random()*COLORS.length)];
     setColor(c);
+  }
+
+  const onClick = (event: any) => {
+    setRipples((currentState) => [ ...currentState, {
+      y: event.clientY,
+      x: event.clientX,
+    }]);
   }
 
   useEffect(() => {
@@ -15,7 +23,14 @@ function App() {
   }, [])
 
   return (
-    <ColorWrapper color={color}></ColorWrapper>
+    <ColorWrapper id="ripple-cursor-wrapper" onClick={onClick} color={color}>
+      {ripples.map((ripple: any) =>
+        <RippleEffect style={{
+          top: `${ripple.y}px`,
+          left: `${ripple.x}px`
+        }} id="ripple-cursor-follow"></RippleEffect>
+      )}
+    </ColorWrapper>
   );
 }
 
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,11 @@
 
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const rippleAnimation = keyframes`
+    0%   { transform: translate(-100%, -100%); }
+    80%  { transform: translate(-100%, -100%) scale(7); }
+    100% { transform: translate(-100%, -100%) scale(7); opacity: 0; }
+`
 
 export const ColorWrapper = styled.main`
     background: ${(props: any) => props.color};
@@ -7,5 +13,20 @@ export const ColorWrapper = styled.main`
     width: 100%;
     height: 100vh;
 
-
+    position: relative;
+    overflow: hidden;
 `
+
+export const RippleEffect = styled.div`
+    width: 50px;
+    height: 50px;
+
+    background: rgba(0,0,0,.2);
+    position: absolute;
+    border-radius: 50%;
+
+    transform: translate(-50%, -50%);
+    animation: .3s linear ${rippleAnimation} forwards;
+`
+
+
